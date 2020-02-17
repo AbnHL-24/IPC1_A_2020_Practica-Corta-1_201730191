3f091ed6a40f6bb931c5e3fd58c0d961e4b34a31
AbnHL-24 <abneralberto-hernandezlopez@cunoc.edu.gt>
2020-02-16 23:59:41 -0600

    Initial commit Se creo el menu del juego.

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String  MENSAJE_BIENVENIDA =  "Bienvenido al juego 3JT, escoja uno de los tres juegos.";
+    public static final String LISTA_DE_JUEGOS = "1.Sopa de letras \n2.Target \n3.2048 \n4.Salir";
+    public static final String ELECCION_INCORRECTA = "leccion incorrecta, por favor ingrese una opcion valida.";
+
+    public static int juegoEscogido;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println(MENSAJE_BIENVENIDA);
+        menuJuegos();
+    }
+
+    public static void menuJuegos() {
+        System.out.println(LISTA_DE_JUEGOS);
+        juegoEscogido = Integer.parseInt(sc.nextLine());
+        if(juegoEscogido ==1) {
+            //sopaLetras.juego();
+            System.out.println("sopaLetras");
+        }
+        else if(juegoEscogido == 2) {
+            //target.juego();
+            System.out.println("target");
+        }
+        else if(juegoEscogido == 3) {
+            //veinteCuarentaOcho.juego();
+            System.out.println("veinteCuarentaOcho");
+        }
+        else if(juegoEscogido == 4) {
+            int cero = 0;
+            System.exit(cero);
+        }
+        else {
+            System.out.println(ELECCION_INCORRECTA);
+            menuJuegos();
+        }
+    }
+}
